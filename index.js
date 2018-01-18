8799c39da2029b6f10d124599979b91dfe346518
David Mendelovits <david.mendelovits@gmail.com>
2018-01-18 15:04:27 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return (`Currently serving ${katzDeliLine.shift()}.`)
+  }
+  else {
+    return ("There is nobody waiting to be served!")
+  }
+}
+
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  }
+
+  const numbersAndNames = [];
+
+  for (let i = 0; i < line.length; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+  return (`The line is currently: ${numbersAndNames.join(', ')}`)
+}
